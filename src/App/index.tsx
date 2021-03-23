6e1a344c4337548f9fd18f7e2622db10562a62e9
Italo Sousa <italo@italosousa.com.br>
2021-03-23 16:25:57 -0300

    Finalizando botoes

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,34 +2,78 @@ import React from 'react';
 import "./styles/styles.css";
 import "./styles/globals.css"
 
+import ReactDOM from 'react-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 function App() {
   return (
     <div className="App">
       <div className="header">
         <img src="https://www.github.com/sousadev.png" alt="" />
-      </div>
-
-      <div className="content">
         <div className="texts">
           <span className="name" >ITALO SOUSA</span>
           <p className="description">Desenvolvedor Web e Mobile</p>
         </div>
       </div>
+
+      <div className="content">
+
+      </div>
       <div className="socials">
-        <div className="card-social">
-          <div>
-            <i></i>
+        <a className="card-social" href="https://www.linkedin.com/in/sousadev">
+          <div >
+          <img src="/assets/linkedin.png" alt="" />
           </div>
           <div>
-            <span>Texto</span>
+            <span>Linkedin</span>
           </div>
-        </div>
-        <div className="card-social">card2</div>
-        <div className="card-social">card3</div>
-        <div className="card-social">card4</div>
+        </a>
+
+        <a className="card-social" href="https://www.instagram.com/italo.dev">
+          <div >
+          <img src="/assets/instagram.png" alt="" />
+          </div>
+          <div>
+            <span>Instagram</span>
+          </div>
+        </a>
+        <a className="card-social" href="https://www.github.com/sousadev">
+          <div >
+          <img src="/assets/github.png" alt="" />
+          </div>
+          <div>
+            <span>GitHub</span>
+          </div>
+        </a>
+        <a className="card-social" href="https://api.whatsapp.com/send/?phone=5511930824454&text&app_absent=0">
+          <div >
+          <img src="/assets/whatsapp.png" alt="" />
+          </div>
+          <div>
+            <span>WhatsApp</span>
+          </div>
+        </a>
+        <a className="card-social" href="https://www.italosousa.com.br">
+          <div >
+          <img src="/assets/web.png" alt="" />
+          </div>
+          <div>
+            <span>Site</span>
+          </div>
+        </a>
+        
+        <a className="card-social" href="tel:+5511930824454">
+          <div >
+          <img src="/assets/call.png" alt="" />
+          </div>
+          <div>
+            <span>Telefone</span>
+          </div>
+        </a>
+        
       </div>
       <div className="footer">
-        <span>Desenvolvido por Italo Sousa</span>
+        <p>Desenvolvido por Italo Sousa</p>
       </div>
     </div>
   );
